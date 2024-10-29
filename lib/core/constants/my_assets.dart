eaa7c0bffc8d98280f5227652916489bada22d0c
cadeion <shatshat000@gmail.com>
2024-10-29 12:08:31 +0530

    Comment Commit

--- lib/core/constants/my_assets.dart
+++ lib/core/constants/my_assets.dart
@@ -1,21 +1,12 @@
-// ignore_for_file: prefer_single_quotes
 class MyAssets {
   MyAssets._();
-  
-  /// Assets for assetsImagesContentCircle
-  /// assets/images/content_circle.png
-  static const String assetsImagesContentCircle = "assets/images/content_circle.png";
 
-  /// Assets for assetsImagesHomeImage
-  /// assets/images/home_image.png
+  static const String assetsImagesContentCircle =
+      "assets/images/content_circle.png";
+
   static const String assetsImagesHomeImage = "assets/images/home_image.png";
 
-  /// Assets for assetsImagesLineCircle
-  /// assets/images/line_circle.png
   static const String assetsImagesLineCircle = "assets/images/line_circle.png";
 
-  /// Assets for assetsImagesRoundBall
-  /// assets/images/round_ball.png
   static const String assetsImagesRoundBall = "assets/images/round_ball.png";
 }
-
--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -17,29 +17,35 @@ class _SplashState extends State<Splash> with TickerProviderStateMixin {
     super.initState();
 
     _controllers = [
-      AnimationController(vsync: this, duration: const Duration(milliseconds: 250)),
-      AnimationController(vsync: this, duration: const Duration(milliseconds: 400)),
-      AnimationController(vsync: this, duration: const Duration(milliseconds: 500)),
-      AnimationController(vsync: this, duration: const Duration(milliseconds: 550)),
-      AnimationController(vsync: this, duration: const Duration(milliseconds: 600)),
+      AnimationController(
+          vsync: this, duration: const Duration(milliseconds: 250)),
+      AnimationController(
+          vsync: this, duration: const Duration(milliseconds: 400)),
+      AnimationController(
+          vsync: this, duration: const Duration(milliseconds: 500)),
+      AnimationController(
+          vsync: this, duration: const Duration(milliseconds: 550)),
+      AnimationController(
+          vsync: this, duration: const Duration(milliseconds: 600)),
     ];
 
     _slideAnimations = _controllers.map((controller) {
       return Tween<Offset>(
-        begin: const Offset(-1.0, 0.004), // Start from slightly below
+        begin: const Offset(-1.0, 0.004),
         end: Offset.zero,
       ).animate(CurvedAnimation(parent: controller, curve: Curves.easeInOut));
     }).toList();
 
     _rotationAnimations = _controllers.map((controller) {
       return Tween<double>(
-        begin: 0.1, // Slight clockwise rotation (in radians)
-        end: 0.0,   // Ends with no rotation
+        begin: 0.1,
+        end: 0.0,
       ).animate(CurvedAnimation(parent: controller, curve: Curves.easeInOut));
     }).toList();
 
     for (int i = 0; i < _controllers.length; i++) {
-      Future.delayed(Duration(milliseconds: i * 100), () => _controllers[i].forward());
+      Future.delayed(
+          Duration(milliseconds: i * 100), () => _controllers[i].forward());
     }
 
     moveToOnboard(context);
@@ -50,12 +56,13 @@ class _SplashState extends State<Splash> with TickerProviderStateMixin {
       Navigator.push(
         context,
         PageRouteBuilder(
-          transitionDuration: const Duration(milliseconds: 800),  // Longer duration for smoothness
+          transitionDuration: const Duration(milliseconds: 800),
           pageBuilder: (context, animation, secondaryAnimation) => Home(),
           transitionsBuilder: (context, animation, secondaryAnimation, child) {
-            const begin = Offset(1.0, 0.0); // Slide in from the right
+            const begin = Offset(1.0, 0.0);
             const end = Offset.zero;
-            final tween = Tween(begin: begin, end: end).chain(CurveTween(curve: Curves.easeInOutCubic));
+            final tween = Tween(begin: begin, end: end)
+                .chain(CurveTween(curve: Curves.easeInOutCubic));
 
             return SlideTransition(
               position: animation.drive(tween),
